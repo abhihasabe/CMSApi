58a94b525ddf71f2324d843e2dab0add13c87429
cmstechsutantra <sumit.deore@techsutantra.com>
2022-04-06 12:59:45 +0530

    make it better

--- src/controllers/employee.controller.js
+++ src/controllers/employee.controller.js
@@ -1,6 +1,6 @@
 const EmployeeModel = require('../models/employee.model');
 
-// create new company
+// // create new company
 // module.exports.createNewEmployee = (req, res) =>{
 //     const employeeReqData = new EmployeeModel(req.body);
 //     console.log('employeeReqData', employeeReqData);
@@ -15,31 +15,31 @@ const EmployeeModel = require('../models/employee.model');
 //     });
 // }
 
-// // get countrys
-// module.exports.authentication = (req, res)=> {
-//     const employeeReqData = new EmployeeModel(req.body);
-//     //console.log('here all Company list');
-//     EmployeeModel.login(employeeReqData, (err, result, token) =>{
-//         if(err){
-//             res.status(500).json({success:0, message:err, data:result});
-//         }else if(result =="User Dose Not Exits"){
-//             res.json({success:0, message:"User Dose Not Exist"});
-//         }else{
-//             res.json({success:1, message:"Data Fetch Successfully",token:token, data :result});
-//         }
-//     })
-// }
+// get countrys
+module.exports.authentication = (req, res)=> {
+    const employeeReqData = new EmployeeModel(req.body);
+    //console.log('here all Company list');
+    EmployeeModel.login(employeeReqData, (err, result, token) =>{
+        // if(err){
+        //     res.status(500).json({success:0, message:err, data:result});
+        // }else if(result =="User Dose Not Exits"){
+        //     res.json({success:0, message:"User Dose Not Exist"});
+        // }else{
+        //     res.json({success:1, message:"Data Fetch Successfully",token:token, data :result});
+        // }
+    })
+}
 
 
 // Refresh token
-module.exports.refreshToken = (req, res)=> {
-    const employeeReqData = new EmployeeModel(req.body);
-    //console.log('here all Company list');
-    EmployeeModel.refresh(employeeReqData, (err, result) =>{
-        if(err){
-            res.status(500).json({success:0, message:err});
-        }else{
-            res.json({success:1, message:"refresh token successfully",token:result});
-        }
-    })
-}
+// module.exports.refreshToken = (req, res)=> {
+//     const employeeReqData = new EmployeeModel(req.body);
+//     //console.log('here all Company list');
+//     EmployeeModel.refresh(employeeReqData, (err, result) =>{
+//         if(err){
+//             res.status(500).json({success:0, message:err});
+//         }else{
+//             res.json({success:1, message:"refresh token successfully",token:result});
+//         }
+//     })
+// }
