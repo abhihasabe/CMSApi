25d818e812017d12c483aedb58a63830c30df360
cmstechsutantra <sumit.deore@techsutantra.com>
2022-04-06 12:35:31 +0530

    make it better

--- src/routes/employee.route.js
+++ src/routes/employee.route.js
@@ -1,19 +1,35 @@
+// const express = require('express');
+// const router = express.Router();
+
+// const employeeController = require('../controllers/employee.controller');
+
+// const checkAuth = require('../../helper/token/token_helper');
+
+// const { addEmployeeValidation } = require('../../validation/employee/employee.validation');
+
+// // create new company
+// router.post('/addEmployee',checkAuth, addEmployeeValidation, employeeController.createNewEmployee);
+
+// // Login
+// router.post('/login',employeeController.authentication);
+
+// // Refresh Token
+// router.post('/refreshToken',employeeController.refreshToken);
+
+// module.exports = router;
+
+
+
 const express = require('express');
 const router = express.Router();
 
-const employeeController = require('../controllers/employee.controller');
+const companysController = require('../controllers/company.controller');
+
+const { addUserValidation } = require('../../validation/users/user.validation');
 
 const checkAuth = require('../../helper/token/token_helper');
 
-const { addEmployeeValidation } = require('../../validation/employee/employee.validation');
-
 // create new company
-//router.post('/addEmployee',checkAuth, addEmployeeValidation, employeeController.createNewEmployee);
-
-// Login
-//router.post('/login',employeeController.authentication);
-
-// Refresh Token
-//router.post('/refreshToken',employeeController.refreshToken);
+router.post('/addCompany', addUserValidation, checkAuth, companysController.createNewCompany);
 
 module.exports = router;
