cc580854e6b5e7dbfd8a77fa707dbae784e4ab30
cmstechsutantra <sumit.deore@techsutantra.com>
2022-04-06 13:35:26 +0530

    make it better

--- src/routes/employees.route.js
+++ src/routes/employees.route.js
@@ -8,6 +8,6 @@ const { employeeValidation } = require('../../validation/employee/employee.valid
 const checkAuth = require('../../helper/token/token_helper');
 
 // create new company
-router.post('/login', employeeValidation, companysController.createNewCompany);
+router.post('/login', companysController.createNewCompany);
 
 module.exports = router;
