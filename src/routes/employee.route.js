e8d7b4b11fd85bbcb19d4919049ee773332e9fc0
cmstechsutantra <sumit.deore@techsutantra.com>
2022-04-06 12:08:08 +0530

    make it better

--- src/routes/employee.route.js
+++ src/routes/employee.route.js
@@ -8,12 +8,12 @@ const checkAuth = require('../../helper/token/token_helper');
 const { addEmployeeValidation } = require('../../validation/employee/employee.validation');
 
 // create new company
-router.post('/addEmployee',checkAuth, addEmployeeValidation, employeeController.createNewEmployee);
+//router.post('/addEmployee',checkAuth, addEmployeeValidation, employeeController.createNewEmployee);
 
 // Login
-router.post('/login',employeeController.authentication);
+//router.post('/login',employeeController.authentication);
 
 // Refresh Token
-router.post('/refreshToken',employeeController.refreshToken);
+//router.post('/refreshToken',employeeController.refreshToken);
 
 module.exports = router;
